00af4d37d91904d49ca16c70ecb66673cb107594
Bruno Andre <bruno_andre@hotmail.com>
2024-01-05 17:25:12 -0300

    Adicionado cor nas peças

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,13 +9,14 @@ try
 
 Tabuleiro tab = new Tabuleiro();
 
-    PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
+tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(0, 0));
+tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
+tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 1));
 
-    Console.WriteLine(pos.toPosicao());
-
-}
-catch(TabuleiroException e)
+    tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3,5));
+    Tela.imprimirTabuleiro(tab);
+}catch(TabuleiroException e)
 {
     Console.WriteLine(e.Message);
 }
--- ConsoleApp1/ConsoleApp1/Tela.cs
+++ ConsoleApp1/ConsoleApp1/Tela.cs
@@ -14,6 +14,7 @@ namespace Xadrez
         {
             for(int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.linhas; j++)
                 {
                     if(tab.peca(i,j) == null)
@@ -22,10 +23,27 @@ namespace Xadrez
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.Write("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
