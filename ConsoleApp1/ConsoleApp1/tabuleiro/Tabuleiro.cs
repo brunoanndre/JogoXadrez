05fb56049bf57c55cc78883e02ae3db32c2e3348
Bruno Andre <bruno_andre@hotmail.com>
2024-01-04 22:09:52 -0300

    Começando a implementação do tabuleiro

---
+++ ConsoleApp1/ConsoleApp1/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez.tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int linhas;
+        public int colunas;
+        public Peca[,] pecas;
+
+        public Tabuleiro()
+        {
+            this.linhas = 8;
+            this.colunas = 8;
+            pecas = new Peca[ linhas, colunas ];
+        }
+    }
+}
