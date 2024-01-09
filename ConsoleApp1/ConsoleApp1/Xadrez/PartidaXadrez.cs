5a4ebc71aa0ccf1104aafed5f998ae3cbcb36325
Bruno Andre <bruno_andre@hotmail.com>
2024-01-09 11:06:06 -0300

    Implementação das regras de movimento

--- ConsoleApp1/ConsoleApp1/Xadrez/PartidaXadrez.cs
+++ ConsoleApp1/ConsoleApp1/Xadrez/PartidaXadrez.cs
@@ -5,8 +5,8 @@ namespace Xadrez
     internal class PartidaXadrez
     {
         public Tabuleiro tab{ get; private  set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual {get; private set; }
         public bool terminada { get; private set; }
         public PartidaXadrez()
         {
@@ -25,6 +25,48 @@ namespace Xadrez
             tab.colocarPeca(p,destino);
         }
 
+        public void realizaJogada(Posicao origem, Posicao destino)
+        {
+            executaMovimento(origem,destino);
+            turno++;
+            mudaJogador();
+        }
+
+        public void mudaJogador()
+        {
+            if(jogadorAtual == Cor.Branca)
+            {
+                jogadorAtual = Cor.Preta;
+            }
+            else
+            {
+                jogadorAtual = Cor.Branca;
+            }
+        }
+
+        public void validarPosicaoDeOrigem(Posicao pos)
+        {
+            if(tab.peca(pos) == null)
+            {
+                throw new TabuleiroException("Não existe peça na posição de origem escolhida!");
+            }
+            if(tab.peca(pos).cor != jogadorAtual)
+            {
+                throw new TabuleiroException("A peça de origem escolhida não é a sua!");
+            }
+            if (!tab.peca(pos).existeMovimentosPossiveis())
+            {
+                throw new TabuleiroException("Não há movimentos possíveis para esta peça!");
+            }
+        }
+
+        public void validarPosicaoDeDestino(Posicao origem, Posicao destino)
+        {
+            if (!tab.peca(origem).podeMoverPara(destino))
+            {
+                throw new TabuleiroException("Posição de destino inválida!");
+            }
+        }
         private void colocarPecas()
         {
             //peças brancas
